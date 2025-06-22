ec19f3c1b93b6a77d598e2bcb65ba9607fcb82df
ysr-otn <yoshihiro.ohtani@gmail.com>
2025-06-22 22:08:58 +0900

    - コメントとエラー処理を修正

--- chatgpt.el
+++ chatgpt.el
@@ -69,6 +69,7 @@
 	 (get-process "chatgpt")
 	 (lambda (p e)
 	   (progn
+		 ;; バッファを *ChatGTP(log-name)* に切り替え
 		 (switch-to-buffer (concat "*ChatGPT(" chatgpt-log-name ")*"))
 		 (end-of-buffer)
 		 (newline)
@@ -87,38 +88,48 @@
 		(newline)
 		(insert "\n----------------------------------------------------------------\n[U]: ")
 		(end-of-buffer))
-	(message "Execute chatgpt-create-new-input command in the buffer of ChatGPT")))
+	;;; *ChatGPT(XXX)* のバッファでなければエラー
+	(message "Execute chatgpt-create-new-input command in the buffer of *ChatGPT(XXX)*")))
 
 ;;; chatgpt-create-new-input-prompt で作成したプロンプトに入力されたユーザメッセージを取得し，
 ;;; ChatGPT へのコマンドラインインタフェースを用いてバッファに結果を表示
 (defun chatgpt-get-new-input-and-output-to-chatgpt-buffer (arg)
   (interactive "p")
+  ;; *ChatGPT(XXX)* のバッファから実行
   (if (string-match "\*ChatGPT\(.*\)\*" (buffer-name))
 	  (progn
 		(goto-char (point-max))
 		(let ((start (search-backward-regexp "^\\[U\\]:" nil t))
 			  (end (point-max))
 			  (buf-name (buffer-name)))
+		  ;;[U]: の入力プロンプト発見
 		  (if start
+			  ;; 入力プロンプトからユーザのメッセージを取得
 			  (let* ((user-message (replace-regexp-in-string "\[U\]: " "" (buffer-substring start (point-max)))))
 				(setq chatgpt-log-name (replace-regexp-in-string "\)\\*" "" (replace-regexp-in-string "\\*ChatGPT\(" "" buf-name)))
 				(end-of-buffer)
 				(newline)
 				(setq chatgpt-user-message user-message)
 				(setq chatgpt-output "")
+				;; 引数が設定されていれば結果をファイルに保存しないよう -n オプションを追加
 				(if (eq arg 4)
 					(start-process "chatgpt" buf-name chatgpt-command "-c" "-n" "-s" chatgpt-log-name user-message)
 				  (start-process "chatgpt" buf-name chatgpt-command "-c" "-s" chatgpt-log-name user-message))
+				;;; chatgpt.py を実行し *ChatGTP(log-name)* バッファに結果を表示
 				(set-process-filter (get-process "chatgpt") 'chatgpt-filter)
 				(set-process-sentinel
 				 (get-process "chatgpt")
 				 (lambda (p e)
 				   (progn
+					 ;; バッファを *ChatGTP(log-name)* に切り替え
 					 (switch-to-buffer (concat "*ChatGPT(" chatgpt-log-name ")*"))
 					 (end-of-buffer)
 					 (insert chatgpt-output))))	
 				(switch-to-buffer buf-name))
-			(message "Insert ChatGPT user message using chatgpt-create-new-input"))))))
+			;; [U]: の入力プロンプトが無ければエラー
+			(message "Insert ChatGPT user message using chatgpt-create-new-input"))))
+	;; *ChatGPT(XXX)* のバッファでなければエラー
+	(message "Execute chatgpt-get-new-input-and-output-to-chatgpt-buffer command in the buffer of *ChatGPT(XXX)*")))
 
 
 (provide 'chatgpt)
